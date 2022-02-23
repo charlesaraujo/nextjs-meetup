12df54da299764a38d232c8df9132755234154ad
Charles Araújo da Silva <charles_araujo@ymail.com>
2022-02-23 18:51:53 -0300

    starting project

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,15 @@
+function MeetupDetail() {
+  return (
+    <>
+      <img
+        src="https://www.euandopelomundo.com/wp-content/uploads/2019/04/sao_paulo.jpg"
+        alt=""
+      />
+      <h1>Meetup in São Paulo</h1>
+      <address>São paulo, 554</address>
+      <p>The meetup description</p>
+    </>
+  );
+}
+
+export default MeetupDetail;
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,6 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+function MeetupDetails() {
+  return <MeetupDetail />;
+}
+
+export default MeetupDetails;
